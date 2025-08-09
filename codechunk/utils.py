bf71cdaa1da3dfa41ef20ca629aa046ae28b30ce
Nabeel KM <nabeelkahlil403@gmail.com>
2025-08-09 10:27:41 +0700

    cleaner log

--- codechunk/utils.py
+++ codechunk/utils.py
@@ -1,7 +1,12 @@
 import logging
 import sys
 
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler('.codechunk.log')
+file_handler.setLevel(logging.DEBUG)
+
 logger = logging.getLogger('codechunk')
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.addHandler(logging.FileHandler('.codechunk.log'))
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
