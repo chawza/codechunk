b1f89bbb8e4ef848d1764ed384050eae850dc729
Nabeel KM <nabeelkahlil403@gmail.com>
2025-08-09 10:30:23 +0700

    is test checking

--- codechunk/utils.py
+++ codechunk/utils.py
@@ -1,10 +1,13 @@
 import logging
 import sys
+import os
+
+IS_TEST = os.environ.get('TEST', '').lower() == 'true' or 'unittest' in sys.argv
 
 stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.INFO if not IS_TEST else logging.NOTSET)
 file_handler = logging.FileHandler('.codechunk.log')
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG if not IS_TEST else logging.NOTSET)
 
 logger = logging.getLogger('codechunk')
 logger.addHandler(stream_handler)
